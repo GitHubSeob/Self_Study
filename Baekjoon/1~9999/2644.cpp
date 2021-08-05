d77c2984f32a02d96d5dc2ad6e26842b414edbda
GitHubSeob <mnrz100@naver.com>
2021-08-05 20:03:57 +0900

    add BOJ_2644

---
+++ Baekjoon/1~9999/2644.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+using namespace std;
+vector<vector<int>>relation;
+vector<bool>visit;
+
+int start;
+int arrive;
+
+int Bfs() {
+    queue<pair<int, int>> q;
+    q.push({ start,0 });
+    while (!q.empty()) {
+        int x = q.front().first;
+        int answer = q.front().second;
+        visit[x] = true;
+        if (x == arrive)
+            return answer;
+        q.pop();
+        for (int cnt = 0; cnt < relation[x].size(); ++cnt) {
+            if(!visit[relation[x][cnt]])
+            q.push({ relation[x][cnt],answer + 1 });
+        }
+    }
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n = 0;
+    int num = 0;
+    cin >> n;
+    cin >> start >> arrive;
+    cin >> num;
+
+    relation.resize(n + 1, vector<int>(0, 0));
+    visit.resize(n + 1, false);
+
+    for (int i = 0; i < num; ++i) {
+        int n1 = 0;
+        int n2 = 0;
+        cin >> n1 >> n2;
+        relation[n1].push_back(n2);
+        relation[n2].push_back(n1);
+        visit[n1] = visit[n2] = false;
+    }
+
+    cout << Bfs();
+}
+
+//문제풀이: https://githubseob.tistory.com/13
