87dcf94e303c55bc39a43323180fd769f2b28031
GitHubSeob <mnrz100@naver.com>
2021-08-14 15:55:41 +0900

    add BOJ_14225

---
+++ Baekjoon/10000~/14225.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N = 0;
+	int i = 0, j = 0;
+	int sum = 0;
+	cin >> N;
+	vector<int>seq(N, 0);
+	int seq_sum;
+	for (i = 0; i < N; ++i) {
+		cin >> seq[i];
+	}
+
+	sort(seq.begin(), seq.end());
+	seq_sum = 0;
+	for (i = 1; i < (1 << N); ++i) {
+		sum = 0;
+		for (j = 0; j < N; ++j) {
+			if (i & (1 << j)) sum += seq[j];
+		}
+		if (seq_sum + 1 < sum) {
+			cout << seq_sum + 1;
+			return 0;
+		}
+		else if (seq_sum + 1 == sum)seq_sum = sum;
+	}
+
+	cout << seq_sum + 1;
+}
+
+//문제풀이: https://githubseob.tistory.com/36
