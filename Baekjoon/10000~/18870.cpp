21b7db2812692cd4c1bd117c655c0e180dbd0161
GitHubSeob <mnrz100@naver.com>
2021-08-31 17:51:32 +0900

    add BOJ_18870

---
+++ Baekjoon/10000~/18870.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0;
+	int idx = 0;
+	cin >> N;
+	vector<int>X(N, 0);
+	vector<int>X2(N, 0);
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> X[idx];
+		X2[idx] = X[idx];
+	}
+	sort(X.begin(), X.end());
+	X.erase(unique(X.begin(), X.end()), X.end());
+
+	for (int idx = 0; idx < X2.size(); ++idx)
+		cout << upper_bound(X.begin(), X.end(), X2[idx]) -
+		(X.begin() + 1) << ' ';
+}
+
+//문제풀이: https://githubseob.tistory.com/65
