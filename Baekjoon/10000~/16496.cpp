4533d9b287c59f0167b189fb9177bd51069e12aa
GitHubSeob <mnrz100@naver.com>
2021-09-17 01:10:23 +0900

    add BOJ_16496

---
+++ Baekjoon/10000~/16496.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool Compare(string s1, string s2) {
+    return s1 + s2 > s2 + s1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+    vector<string>Arr(N,"");
+    int idx = 0;
+
+    for (idx = 0; idx < N; ++idx)
+        cin >> Arr[idx];
+
+    sort(Arr.begin(), Arr.end(), Compare);
+    if (Arr[0] == "0") cout << "0";
+    else
+        for (idx = 0; idx < Arr.size(); ++idx)
+            cout << Arr[idx];
+}
+
+//문제풀이: https://githubseob.tistory.com/113
