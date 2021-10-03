50b55d8f29a03e10d5292e798108cbd1857a5126
GitHubSeob <mnrz100@naver.com>
2021-10-03 20:36:06 +0900

    add BOJ_13419

---
+++ Baekjoon/10000~/13419.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int T(0);
+	string data("");
+
+	int idx(0);
+	cin >> T;
+	while (T--) {
+		cin >> data;
+		string first("");
+		string second("");
+		for (idx = 0; idx < data.size(); ++idx) {
+			if (idx % 2 == 0) first += data[idx];
+			else second += data[idx];
+		}
+		if (data.size() % 2 == 1) {
+			string copy(first);
+			first += second;
+			second += copy;
+		}
+		cout << first << '\n' << second << '\n';
+	}
+}
+
+//문제풀이: https://githubseob.tistory.com/123
