5e9c370c949f89c563234724812cea38a3b0435a
GitHubSeob <githubseob@gmail.com>
2022-04-15 00:03:27 +0900

    add Programmers

---
+++ Programmers/Level 2/더 맵게/Solution.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    int idx(0);
+    int num1(0), num2(0);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (idx = 0; idx < scoville.size(); ++idx) {
+        pq.push(scoville[idx]);
+    }
+
+    while (pq.size() > 1) {
+        if (pq.top() >= K)
+            break;
+        num1 = pq.top();
+        pq.pop();
+        num2 = pq.top();
+        pq.pop();
+        pq.push(num1 + num2 * 2);
+        answer++;
+    }
+
+    if (pq.top() < K)
+        return -1;
+
+    return answer;
+}
---
+++ Programmers/Level 2/짝지어 제거하기/Solution.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char>st;
+    int answer = -1;
+    int idx(0);
+
+    for (idx = 0; idx < s.size(); ++idx) {
+        if (!st.empty()) {
+            if (st.top() == s[idx])
+                st.pop();
+            else
+                st.push(s[idx]);
+        }
+        else st.push(s[idx]);
+    }
+    if (!st.empty())
+        answer = 0;
+    else
+        answer = 1;
+
+    return answer;
+}
