ecbcfe4fd1eb7968ddad72d5d173a4cedfcdcd76
GitHubSeob <githubseob@gmail.com>
2022-04-17 21:25:56 +0900

    add Programmers

---
+++ Programmers/Level 2/메뉴 리뉴얼/Solution.cpp
@@ -0,0 +1,53 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<unordered_map<string, int>>vm;
+
+bool cmp(pair<string, int>num1, pair<string, int>num2) {
+    return num1.second > num2.second;
+}
+
+void DFS(int idx, int cnt, vector<int>course, string mkset, string menu) {
+    if (idx == menu.size() || cnt == course[course.size() - 1]) {
+        int idx2(0);
+        for (idx2 = 0; idx2 < course.size(); ++idx2) {
+            if (cnt == course[idx2]) {
+                vm[idx2][mkset]++;
+            }
+        }
+    }
+    else if (idx < menu.size() && cnt < course[course.size() - 1]) {
+        DFS(idx + 1, cnt, course, mkset, menu);
+        mkset += menu[idx];
+        DFS(idx + 1, cnt + 1, course, mkset, menu);
+    }
+}
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    vm = vector<unordered_map<string, int>>(course.size());
+    int idx(0), idx2(0);
+    for (idx = 0; idx < orders.size(); ++idx) {
+        sort(orders[idx].begin(), orders[idx].end());
+        DFS(0, 0, course, "", orders[idx]);
+    }
+
+    for (idx = 0; idx < vm.size(); ++idx) {
+        vector<pair<string, int>>v(vm[idx].begin(), vm[idx].end());
+        sort(v.begin(), v.end(), cmp);
+        for (idx2 = 0; idx2 < v.size(); ++idx2) {
+            if (v[idx2].second > 1) {
+                answer.push_back(v[idx2].first);
+                if (idx2 + 1 == v.size() || v[idx2].second != v[idx2 + 1].second)
+                    break;
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+    return answer;
+}
---
+++ Programmers/Level 2/행렬 테두리 회전하기/Solution.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer(queries.size(), 10000);
+
+    int y = 0, x = 0;
+    int idx(0);
+    int s_x(0), e_x(0), s_y(0), e_y(0);
+
+    vector<vector<int>>matrix(rows, vector<int>(columns, 0));
+
+    idx = 0;
+    for (y = 0; y < rows; ++y) {
+        for (x = 0; x < columns; ++x) {
+            matrix[y][x] = ++idx;
+        }
+    }
+
+    vector<vector<int>>copy_matrix = matrix;
+
+    for (idx = 0; idx < queries.size(); ++idx) {
+        s_x = queries[idx][1] - 1;
+        e_x = queries[idx][3] - 1;
+        s_y = queries[idx][0] - 1;
+        e_y = queries[idx][2] - 1;
+        y = s_y;
+
+        for (x = s_x + 1; x <= e_x; ++x) {
+            matrix[y][x] = copy_matrix[y][x - 1];
+            answer[idx] = min(answer[idx], matrix[y][x]);
+        }
+        --x;
+        ++y;
+        
+        for (y; y <= e_y; ++y) {
+            matrix[y][x] = copy_matrix[y - 1][x];
+            answer[idx] = min(answer[idx], matrix[y][x]);
+        }
+        --y;
+        --x;
+        
+        for (x; x >= s_x; --x) {
+            matrix[y][x] = copy_matrix[y][x + 1];
+            answer[idx] = min(answer[idx], matrix[y][x]);
+        }
+        ++x;
+        --y;
+        
+        for (y; y >= s_y; --y) {
+            matrix[y][x] = copy_matrix[y + 1][x];
+            answer[idx] = min(answer[idx], matrix[y][x]);
+        }
+        copy_matrix = matrix;
+    }
+
+    return answer;
+}
