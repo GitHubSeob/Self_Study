80f2aa4250b69bbbd47b02c8fec2e2efa0f2d523
GitHubSeob <githubseob@gmail.com>
2022-04-23 00:16:06 +0900

    add Programmers

---
+++ Programmers/Level 2/카펫/Solution.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer(2, 0);
+    int x(0), y(0);
+    int idx(0);
+    int sum(brown + yellow);
+    vector<int>div;
+    for (x = sum; x >= 1; --x) {
+        if (sum % x == 0) {
+            if (yellow == ((x - 2) * (sum / x - 2))) {
+                answer[0] = x;
+                answer[1] = sum / x;
+                return answer;
+            }
+        }
+    }
+}
