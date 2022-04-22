3f7a9fd653cdae9a9edfb6ffa1c7df916a291cb3
GitHubSeob <githubseob@gmail.com>
2022-04-22 23:34:34 +0900

    add Programmers

---
+++ Programmers/Level 2/H-Index/Solution.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int idx(0);
+    sort(citations.begin(), citations.end());
+
+    for (idx = 0; idx < (citations.size() + 1) / 2; ++idx) {
+        if (citations.size() - idx <= citations[idx]) {
+            return (citations.size() - idx);
+        }
+    }
+}
