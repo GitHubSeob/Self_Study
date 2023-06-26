076bd6955569be47981efef26cb38000bf60ae1a
GitHubSeob <githubseob@gmail.com>
2023-06-27 05:13:12 +0900

    add Programmers

---
+++ Programmers/Level 3/거스름돈/Solution.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#define DIV 1000000007
+using namespace std;
+
+int solution(int n, vector<int> money) {
+    int idx(0), won(0);
+    vector<int>cnt(n + 1, 0);
+
+    cnt[0] = 1;
+
+    for (won = 0; won < money.size(); ++won) {
+        for (idx = money[won]; idx <= n; ++idx) {
+            cnt[idx] += (cnt[idx - money[won]] % DIV);
+        }
+    }
+
+    return cnt[n];
+}
