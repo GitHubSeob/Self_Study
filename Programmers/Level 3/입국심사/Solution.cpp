7b2df600a6b6024954154bdbc8240b9364ff7fcd
GitHubSeob <githubseob@gmail.com>
2023-06-29 03:57:19 +0900

    add Programmers

---
+++ Programmers/Level 3/입국심사/Solution.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#define MAX 1000000000000000000
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long left(0), right(MAX), mid(0), idx(0), cnt(0);
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+        cnt = 0;
+        for (idx = 0; idx < times.size(); ++idx) {
+            cnt += mid / times[idx];
+        }        
+        if (cnt < n) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
