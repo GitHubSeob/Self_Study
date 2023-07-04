4d92a876f9d73daa72eaf2a40b26e048f9e6bfd4
GitHubSeob <githubseob@gmail.com>
2023-07-04 22:37:30 +0900

    add BOJ_14916

---
+++ Baekjoon/10000~/14916.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int idx(0), idx2(0), N(0);
+	cin >> N;
+	vector<int>won(N + 1, -1);
+	vector<int>coin = { 2,5 };
+
+	won[0] = 0;
+
+	for (idx = 0; idx < coin.size(); ++idx) {
+		for (idx2 = 1; idx2 <= N; ++idx2) {
+			if (idx2 >= coin[idx] && won[idx2 - coin[idx]] != -1) {
+				won[idx2] = won[idx2 - coin[idx]] + 1;
+			}
+		}
+	}
+
+	cout << won[N];
+}
+
+//문제풀이: https://githubseob.tistory.com/266
