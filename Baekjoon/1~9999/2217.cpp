16778e67a1e4f2999d7eed592baa4b139cba50c1
GitHubSeob <githubseob@gmail.com>
2023-07-04 22:37:07 +0900

    add BOJ_2217

---
+++ Baekjoon/1~9999/2217.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), idx(0), answer(0);
+
+	cin >> N;
+	vector<int>rope(N, 0);
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> rope[idx];
+	}
+
+	sort(rope.begin(), rope.end(), greater<int>());
+
+	for (idx = 0; idx < N; ++idx) {
+		answer = max(answer, rope[idx] * (idx + 1));
+	}
+	cout << answer;
+}
+
+//문제풀이: https://githubseob.tistory.com/268
