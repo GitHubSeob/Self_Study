5239935ca53c8d61666d5775ad137f66306680ea
GitHubSeob <githubseob@gmail.com>
2023-07-04 22:37:16 +0900

    add BOJ_11000

---
+++ Baekjoon/10000~/11000.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), idx(0), answer(0), cnt(0);
+	cin >> N;
+	vector<pair<int, int>>course(N);
+	priority_queue<int, vector<int>, greater<int>>pq;
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> course[idx].first >> course[idx].second;
+	}
+
+	sort(course.begin(), course.end());
+
+	for (idx = 0; idx < N; ++idx) {
+		while (!pq.empty() && course[idx].first >= pq.top()) {
+			pq.pop();
+		}
+		if (pq.empty() || course[idx].first < pq.top()) {
+			pq.push(course[idx].second);
+		}
+		cnt = pq.size();
+		answer = max(answer, cnt);
+	}
+	cout << answer;
+}
+
+//문제풀이: https://githubseob.tistory.com/272
