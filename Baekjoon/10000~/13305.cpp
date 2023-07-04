3ec8ee380f8678664124269268046126f32ca57d
GitHubSeob <githubseob@gmail.com>
2023-07-04 22:37:23 +0900

    add BOJ_13305

---
+++ Baekjoon/10000~/13305.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#define MAX 1000000000
+#define ll long long
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	ll idx(0), N(0), min_price(MAX), answer(0);
+	cin >> N;
+	vector<ll>road(N - 1, 0);
+	vector<ll>price(N, 0);
+
+	for (idx = 0; idx < N - 1; ++idx) {
+		cin >> road[idx];
+	}
+	for (idx = 0; idx < N; ++idx) {
+		cin >> price[idx];
+	}
+
+	for (idx = 0; idx < N - 1; ++idx) {
+		min_price = min(min_price, price[idx]);
+		answer += min_price * road[idx];
+	}
+
+	cout << answer;
+}
+
+//문제풀이: https://githubseob.tistory.com/269
