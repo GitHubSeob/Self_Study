6625f659ae122ae15b7e1dc2771568409ddd48a7
GitHubSeob <githubseob@gmail.com>
2023-07-06 00:27:35 +0900

    add BOJ_20300

---
+++ Baekjoon/10000~/20300.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+#define END loss.size() -1
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	ll N(0), idx(0), answer(0);
+
+	cin >> N;
+	vector<ll>loss(N, 0);
+	for (idx = 0; idx < N; ++idx) {
+		cin >> loss[idx];
+	}
+
+	sort(loss.begin(), loss.end());
+
+	if (N % 2 == 0) {
+		for (idx = 0; idx < N/2; ++idx) {
+			answer = max(answer, loss[idx] + loss[END - idx]);
+		}
+	}
+	else if (N % 2 != 0) {
+		answer = loss[END];
+		for (idx = 0; idx < N / 2; ++idx) {
+			answer = max(answer, loss[idx] + loss[END - 1 - idx]);
+		}
+	}
+	cout << answer;
+}
+
+//문제풀이: https://githubseob.tistory.com/275
