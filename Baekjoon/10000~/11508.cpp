659621a0d69875ea9cda309935483711c3f8219d
GitHubSeob <githubseob@gmail.com>
2023-07-06 00:27:27 +0900

    add BOJ_11508

---
+++ Baekjoon/10000~/11508.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), idx(0), answer(0);
+
+	cin >> N;
+	vector<int>cost(N, 0);
+
+	for (idx = 0; idx < N; ++idx) {
+		cin >> cost[idx];
+	}
+	sort(cost.begin(), cost.end(), greater<int>());
+
+	for (idx = 0; idx < N; ++idx) {
+		if ((idx + 1) % 3 != 0) {
+			answer += cost[idx];
+		}
+	}
+	cout << answer;
+}
+
+//문제풀이: https://githubseob.tistory.com/274
