19016ef51fc690f60bceac98d7e0557330240fcd
GitHubSeob <githubseob@gmail.com>
2023-07-09 01:50:25 +0900

    add Programmers

---
+++ Programmers/Level 3/보석 쇼핑/Solution.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    int idx(0), start(0);
+    vector<int> answer(2, 0);
+    map<string, int>gem_cnt;
+
+    for (idx = 0; idx < gems.size(); ++idx) {
+        if (gem_cnt[gems[idx]] == 0) {
+            ++gem_cnt[gems[idx]];
+            answer[0] = start;
+            answer[1] = idx;
+        }
+        else {
+            ++gem_cnt[gems[idx]];
+            while (gem_cnt[gems[start]] > 1) {
+                --gem_cnt[gems[start]];
+                ++start;
+            }
+            if (answer[1] - answer[0] > idx - start) {
+                answer[0] = start;
+                answer[1] = idx;
+            }
+        }
+
+    }
+
+    ++answer[0];
+    ++answer[1];
+    return answer;
+}
---
+++ Programmers/Level 3/스티커 모으기(2)/Solution.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+using namespace std;
+
+int solution(vector<int> sticker)
+{
+    int idx(0), answer(sticker[0]);
+
+    sticker.insert(sticker.begin(), 0);
+    sticker.insert(sticker.begin(), 0);
+    sticker.insert(sticker.begin(), 0);
+
+    vector<int>DP1(sticker.size(), 0);
+
+    for (idx = 3; idx + 1 < sticker.size(); ++idx) {
+        DP1[idx] = sticker[idx] + max(DP1[idx - 2], DP1[idx - 3]);
+        answer = max(answer, DP1[idx]);
+    }
+
+    sticker.erase(sticker.begin() + 3);
+
+    vector<int>DP2(sticker.size(), 0);
+
+    for (idx = 3; idx < sticker.size(); ++idx) {
+        DP2[idx] = sticker[idx] + max(DP2[idx - 2], DP2[idx - 3]);
+        answer = max(answer, DP2[idx]);
+    }
+
+    return answer;
+}
