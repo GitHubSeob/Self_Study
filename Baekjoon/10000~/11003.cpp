2af0fc0b2d4c12df16ab420d8925fdfc6a7de5db
GitHubSeob <githubseob@gmail.com>
2023-08-17 03:27:49 +0900

    add BOJ_11003

---
+++ Baekjoon/10000~/11003.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+#define pii pair<int, int>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N(0), L(0);
+	cin >> N >> L;
+	vector<int>nums(N, 0);
+	
+	priority_queue<pii, vector<pii>, greater<pii>>pq;
+	for (int idx = 0; idx < N; ++idx) {
+		cin >> nums[idx];
+		pq.push({ nums[idx],idx });
+		if (idx == 0) {			
+			cout << nums[idx] << " ";
+		}
+		else {			
+			while (1) {
+				if (pq.top().second < idx - L + 1) pq.pop();
+				else break;
+			}
+			cout << pq.top().first <<' ';
+		}		
+	}	
+}
+
+//문제풀이: https://githubseob.tistory.com/311
