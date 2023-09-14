b56a4c0e63bc0991a2d74f8728d72e9c0ed9824b
GitHubSeob <githubseob@gmail.com>
2023-09-15 06:45:51 +0900

    add BOJ_18917

---
+++ Baekjoon/10000~/18917.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int M(0);
+	cin >> M;
+
+	ll cmd(0), num(0);
+	ll sum(0), XOR(0);
+
+	for (int idx = 0; idx < M; ++idx) {
+		cin >> cmd;
+		if (cmd == 1) {
+			cin >> num;
+			XOR ^= num;
+			sum += num;
+		}
+		else if (cmd == 2) {
+			cin >> num;
+			sum -= num;
+			XOR ^= num;
+		}
+		else if (cmd == 3) {
+			cout << sum << '\n';
+		}
+		else if (cmd == 4) {
+			cout << XOR << '\n';
+		}
+	}
+}
+
+//문제풀이: https://githubseob.tistory.com/341
